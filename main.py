9735b280b02fb1a796efd03428b53293b6eebf5d
Tothinator <dtoth2016@gmail.com>
2020-08-20 15:57:15 -0500

    Made the endpoints post and returning sorted arrays and time to sort

--- main.py
+++ main.py
@@ -1,51 +1,50 @@
 import time
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 import numpy
 
 app = FastAPI()
 np = numpy
 
-a = np.array([4, 5, 2, 1, 3])
-
 @app.get('/')
 async def root():
     return {"message": "Hello World"}
 
-@app.get('/quicksort')
-async def sort():
+@app.post('/quicksort')
+async def quicksort(a: list):
     start = time.perf_counter()
-    array = sort_array(a)
+    array = sort_array(np.array(a))
     stop = time.perf_counter()
     return {
         "sorted_array": str(array),
         "time": stop - start
     }
 
-@app.get('/mergesort')
-async def sort():
+@app.post('/mergesort')
+async def mergesort(a: list):
     start = time.perf_counter()
-    array = sort_array(a, 'mergesort')
+    array = sort_array(np.array(a), 'mergesort')
     stop = time.perf_counter()
     return {
         "sorted_array": str(array),
         "time": stop - start
     }
 
-@app.get('/heapsort')
-async def sort():
+@app.post('/heapsort')
+async def heapsort(a: list):
     start = time.perf_counter()
-    array = sort_array(a, 'heapsort')
+    array = sort_array(np.array(a), 'heapsort')
     stop = time.perf_counter()
     return {
         "sorted_array": str(array),
         "time": stop - start
     }
 
-@app.get('/stablesort')
-async def sort():
+@app.post('/stablesort')
+async def stablesort(a: list):
     start = time.perf_counter()
-    array = sort_array(a, 'stable')
+    array = sort_array(np.array(a), 'stable')
     stop = time.perf_counter()
     return {
         "sorted_array": str(array),
@@ -53,4 +52,5 @@ async def sort():
     }
 
 def sort_array(array, sort_type = None):
+    print(array)
     return np.sort(array, kind=sort_type)
